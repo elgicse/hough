e6f194e2b5754a1978b7a003b6d8ea4fb9493e5f
elgicse <elena.graverini@gmail.com>
2014-02-14 11:56:30 +0100

    added tracklet funcs (empty)

--- hough.py
+++ hough.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import ROOT as r
 import numpy as np
+import itertools as itools
 import sys
 
 inrootfile = "data/UTHits.root"
@@ -8,6 +9,7 @@ outrootfile = "data/out/out.root"
 
 TREENAME = "UTHits/MatchedTracks"
 XY,XZ,YZ = 0,1,2
+YX,ZX,ZY = 0,1,2
 X,Y,Z = 0,1,2
 
 class mydict(dict):
@@ -24,32 +26,35 @@ def matrixInit():
     return np.matrix()
 
 def isGood(track):
-    return track.reconstructible_asLong and track.true_p > 3000 and track.true_pt > 500 and track.true_eta > 2 and track.true_eta < 5 and abs(track.pid) != 22 and track.fromB
+    return (track.reconstructible_asLong 
+        and track.true_p > 3000 
+        and track.true_pt > 500 
+        and track.true_eta > 2 
+        and track.true_eta < 5 
+        and abs(track.pid) != 22 
+        and track.fromB)
 
 def HGraph(n,x,y):
     gr = r.TGraph(n,x,y)
-    gr.SetMarkerStyle(7)
-    #gr.SetLineColor()
-    #gr.SetMarkerColor()
+    gr.SetMarkerStyle(20)
     gr.SetName("tracks_graph") 
     gr.SetTitle("tracks_graph") 
-    return gROOT    
+    return gr
 
 def hitGraph(track,plane):
     if plane == XY:
-        gr = HGraph(track.vplen,track.velo_x_hit,track.velo_y_hit)
+        gr = HGraph(track.vplen, track.velo_x_hit, track.velo_y_hit)
     if plane == XZ:
-        gr = HGraph(track.vplen,track.velo_z_hit,track.velo_x_hit)   
+        gr = HGraph(track.vplen, track.velo_z_hit, track.velo_x_hit)   
     if plane == YZ:
-        gr = HGraph(track.vplen,track.velo_z_hit,track.velo_y_hit)   
+        gr = HGraph(track.vplen, track.velo_z_hit, track.velo_y_hit)   
     return gr
 
-
 def createHitsGraph(mg,tracks):
-    for t in filter(isGood,tracks):
-            mg[XY].Add(hitGraph(t,XY))
-            mg[XZ].Add(hitGraph(t,XZ))
-            mg[YZ].Add(hitGraph(t,YZ))
+    for t in itools.ifilter(isGood,tracks):
+        mg[XY].Add(hitGraph(t,XY))
+        mg[XZ].Add(hitGraph(t,XZ))
+        mg[YZ].Add(hitGraph(t,YZ))
     return True
 
 def getXYZ(hit,track):
@@ -57,8 +62,6 @@ def getXYZ(hit,track):
     x = track.velo_x_hit[index]
     y = track.velo_y_hit[index]
     z = track.velo_z_hit[index]
-    #x,y,z = 0,0,0 #calc x,y,z
-    hit = x,y,z
     return x,y,z
 
 def calcRho(plane,th,x,y,z):
@@ -74,28 +77,27 @@ def transform(x1,x2,theta):
     return x1*np.cos(theta) + x2*np.sin(theta)
 
 def setDictionaries(dictionaries,tracks):
-    for t in tracks:
-        if isGood(t):
-            #"GET HIT LIST"
-            hits = []*track.vplen
-            for h in hits:
-                x,y,z = getXYZ(h,track)
-                rho = [],[],[]  
-                for th in theta:
-                    rho[XY] = calcRho(XY,th,x,y,z)
-                    dictionaries[XY].setdefault((rho[XY],th),dict()).setdefault(X,list()).append(x)
-                    dictionaries[XY].setdefault((rho[XY],th),dict()).setdefault(Y,list()).append(y)
-                    dictionaries[XY].setdefault((rho[XY],th),dict()).setdefault(Z,list()).append(z)
+    for t in itools.ifilter(isGood,tracks):
+        #"GET HIT LIST"
+        hits = []*track.vplen
+        for h in hits:
+            x,y,z = getXYZ(h,t)
+            rho = [],[],[]  
+            for th in theta:
+                rho[XY] = calcRho(XY,th,x,y,z)
+                dictionaries[XY].setdefault((rho[XY],th),dict()).setdefault(X,list()).append(x)
+                dictionaries[XY].setdefault((rho[XY],th),dict()).setdefault(Y,list()).append(y)
+                dictionaries[XY].setdefault((rho[XY],th),dict()).setdefault(Z,list()).append(z)
 
-                    rho[XZ] = calcRho(XZ,th,x,y,z)
-                    dictionaries[XZ].setdefault((rho[XZ],th),dict()).setdefault(X,list()).append(x)
-                    dictionaries[XZ].setdefault((rho[XZ],th),dict()).setdefault(Y,list()).append(y)
-                    dictionaries[XZ].setdefault((rho[XZ],th),dict()).setdefault(Z,list()).append(z)
+                rho[XZ] = calcRho(XZ,th,x,y,z)
+                dictionaries[XZ].setdefault((rho[XZ],th),dict()).setdefault(X,list()).append(x)
+                dictionaries[XZ].setdefault((rho[XZ],th),dict()).setdefault(Y,list()).append(y)
+                dictionaries[XZ].setdefault((rho[XZ],th),dict()).setdefault(Z,list()).append(z)
 
-                    rho[YZ] = calcRho(YZ,th,x,y,z)
-                    dictionaries[YZ].setdefault((rho[YZ],th),dict()).setdefault(X,list()).append(x)
-                    dictionaries[YZ].setdefault((rho[YZ],th),dict()).setdefault(Y,list()).append(y)
-                    dictionaries[YZ].setdefault((rho[YZ],th),dict()).setdefault(Z,list()).append(z)
+                rho[YZ] = calcRho(YZ,th,x,y,z)
+                dictionaries[YZ].setdefault((rho[YZ],th),dict()).setdefault(X,list()).append(x)
+                dictionaries[YZ].setdefault((rho[YZ],th),dict()).setdefault(Y,list()).append(y)
+                dictionaries[YZ].setdefault((rho[YZ],th),dict()).setdefault(Z,list()).append(z)
     return True
 
 def dict2Matrix(dictionaries,matrices):
@@ -112,12 +114,19 @@ def createHoughGraph(mg,tracks):
     dict2Matrix(Dictionaries,Matrices)
     searchPeaks(Matrices,PeakLists)
     calcParams(PeakLists,ParamLists)
-    "create graphs"
-    "add graph in multigraf"
-    "save in root file"
-    "create DTP Dictionary"
+    #"create graphs"
+    #"add graph in multigraph"
+    #"save in root file"
+    #"create DTP Dictionary"
     return True
 
+def makeTracklets(params,trackletsContainer):
+    pass
+
+def matchTracklets(trackletsContainer,dictionaries,matchedTracklets):
+    pass
+
+#################### MAIN PROGRAM ##############################
 
 if __name__ == "__main__":
     print "HOUGH START..."       
@@ -128,11 +137,11 @@ if __name__ == "__main__":
     MultiGraphs = mgInit(),mgInit(),mgInit()
     PeakLists = [],[],[]
     ParamLists = [],[],[]
+    Tracklets = [],[],[] #tracks on every plane
+    Matched = [],[],[] #tracks on every plane
     Dictionaries = mydict(),mydict(),mydict()
-    #Matrices = matrixInit(),matrixInit(),matrixInit()
+    Matrices = matrixInit(),matrixInit(),matrixInit()
     theta = range(180)
-
-
 
 
         #root file descriptor
@@ -143,12 +152,22 @@ if __name__ == "__main__":
         #get tree
     Tracks = ifd.Get(TREENAME)
 
-    if not createHitsGraph(MultiGraphs,Tracks):
+    if not createHitsGraph(MultiGraphs, Tracks):
         print "ERROR: createHitsGraph"
         sys.exit(1)
-    sys.exit(0)
-    if not createHoughGraph(MultiGraphs,Tracks):
+    
+    sys.exit(0) #debug
+    
+    if not createHoughGraph(MultiGraphs, Tracks):
         print "ERROR: createHoughGraph"
+        sys.exit(1)
+    
+    if not makeTracklets(ParamLists, Tracklets):
+        print "ERROR: makeTracklets"
+        sys.exit(1)
+
+    if not matchTracklets(Tracklets, Dictionaries, Matched):
+        print "ERROR: matchTracklets"
         sys.exit(1)
 
     sys.exit(0)
